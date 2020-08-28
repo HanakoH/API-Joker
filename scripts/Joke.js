fd386698538a18d10c3989c003b34993960e2ef2
Hanako Hashiguchi <hanako.e.hashiguchi@gmail.com>
2020-08-28 14:43:06 -0500

    should have done this sooner, but got through the first challenge

--- scripts/Joke.js
+++ scripts/Joke.js
@@ -1,2 +1,16 @@
 // Define and export a function called Joke.
 // It should accept a joke object as a parameter and return an html string
+export const JokeSetup = (joke) => {
+    return `
+    <section class="theJokeSetup">
+        <div class="setup">${joke.setup}</div>
+    </section>
+    `
+}
+export const JokePunchline = (joke) => {
+    return `
+    <section class="theJokePunchline">
+        <div class="punchline">${joke.punchline}</div>
+    </section>
+    `
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,28 @@
-const jokeBtn = document.querySelector(".request-joke-btn");
+import { getJoke } from './JokeProvider.js';
+import { JokeSetup, JokePunchline } from './Joke.js';
 
-jokeBtn.addEventListener("click", (e) => {
-  console.log("I should be grabbing a joke now...");
-  // TODO: fetch a joke from the Joke API and render it to the DOM
+const jokeBtn = document.querySelector(".request-joke-btn");
+const punchlineBtn = document.querySelector(".request-punchline-btn");
+
+let theJoke = {};
+
+jokeBtn.addEventListener("click", () => {
+  getJoke()
+  .then(jokeData => {
+    theJoke = jokeData;
+    const fullJokeSetup = JokeSetup(theJoke)
+    const jokeSetupElement = document.querySelector(".joke-setup");
+    jokeSetupElement.innerHTML = ""
+    jokeSetupElement.innerHTML += fullJokeSetup
+  })
 });
+
+
+
+punchlineBtn.addEventListener("click", () => {
+    const fullJokePunchline = JokePunchline(theJoke)
+    const jokePunchlineElement = document.querySelector(".joke-punchline");
+    jokePunchlineElement.innerHTML = ""
+    jokePunchlineElement.innerHTML += fullJokePunchline
+});
+
